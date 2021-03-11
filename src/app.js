480aa8dda65e98c624ef9a04d7a84010f0d29254
Bhargav <sbhargav670@gmail.com>
2021-03-11 17:25:20 +0000

    First commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// import module elements
+import { LightningElement } from 'lwc';
+// declare class to expose the component
+export default class App extends LightningElement {
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
+   ready = false;
+   // It is one of our lifecycle hooks.
+   // The method is triggered when a component is inserted in the document object model (DOM)
+   connectedCallback() {
+       setTimeout(() => {
+           this.ready = true;
+       }, 3000);
+   }
+}
